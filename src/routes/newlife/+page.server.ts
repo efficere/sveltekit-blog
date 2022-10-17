029fd742a65fa9b1c214a6c8869e80cdd82969a8
Michel <mfagundesleal@outlook.com>
2022-10-17 16:55:04 -0300

    Sistema para busca de clientes online em /newlife

---
+++ src/routes/newlife/+page.server.ts
@@ -0,0 +1,44 @@
+/** @type {import('./$types').Actions} */
+
+// lida com o formulário e executa o fetch
+export const actions: import('./$types').Actions = {
+    default: async({ request }) => {
+        // pega o CPF passado pelo input
+        const formData = await request.formData();
+        const cpf = formData.get('cpf');
+        let doc: FormDataEntryValue | null;
+        if(Number(cpf) > 999){
+            doc = cpf;
+        }else{
+            doc = "erro";
+        }
+
+        console.log(doc);
+        // pega o token
+        const getToken = async () => {
+            const tokenRes = await fetch(`https://sac.newlifefibra.com.br/mk/WSAutenticacao.rule?sys=MK0&token=004e1e43eeabb77526992637db07635a&password=3687777e73e3415&cd_servico=9999`);
+            const tokenData = await tokenRes.json();
+            return tokenData.Token;
+        }
+    
+        // consulta a API em busca do nome na base de clientes
+        // passa o token para a variável
+        const token = await getToken();
+        const clientRes = await fetch(`https://sac.newlifefibra.com.br/mk/WSMKConsultaClientes.rule?sys=MK0&token=${token}&doc=${doc}`);
+        const clientData = await clientRes.json();
+
+        let erro: Boolean;
+        if(!clientRes.ok || clientData.status === 'ERRO'){
+            erro = true;
+        }else{
+            erro = false
+        }
+        
+        if(!erro){
+            return { clientData }
+        }else if(erro){
+            return { erro}
+        }
+    
+    }
+}
---
+++ src/routes/newlife/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	/** @type {import('./$types').ActionData} */
+	export let form;
+	let ativo = false;
+</script>
+2
+<div class="container mx-auto flex items-center justify-center h-screen w-screen">
+	<form method="POST" class="flex flex-col gap-20 min-h-[20rem] min-w-[20rem] border border-black">
+		<div class="logos">
+			<img src="/logo-azul.webp" class="max-w-[12.5rem]" alt="">
+			<img src="" alt="">
+		</div>
+		<div class="body flex flex-col gap-10 text-center">
+			<fieldset class="flex flex-col gap-2 items-center w-full">
+				<label for="cpf">CPF (somente números, sem pontuação)</label>
+				<input type="text" id="cpf" name="cpf" placeholder="Digite o CPF somente números..." class="w-full" required pattern="^[0-9]{'{11}'}$" maxlength="11">
+			</fieldset>
+			<button type="submit">Enviar dados</button>
+		</div>
+		{#if form?.clientData && !form?.erro}
+			<p class="text-green-500"><span class="font-bold">{form?.clientData[0].Nome}</span> é cliente ativo New Life Fibra</p>
+		{:else if form?.erro}
+			<p class="text-red-500">CPF não encontrado em nossa base de dados</p>
+		{/if}
+	</form>
+</div>

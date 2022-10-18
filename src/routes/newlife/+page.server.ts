e305cec3b6b0362d5998f39a787094ef6ee82b3e
Michel <mfagundesleal@outlook.com>
2022-10-18 15:36:31 -0300

    Comentários inseridos para documentação do código

--- src/routes/newlife/+page.server.ts
+++ src/routes/newlife/+page.server.ts
@@ -6,24 +6,33 @@ export const actions: import('./$types').Actions = {
         // pega o CPF passado pelo input
         const formData = await request.formData();
         const cpf = formData.get('cpf');
+
+        // trata o CPF informado (caso menor que 999 passa um valor não esperado no endpoint, o que retorna em erro)
         let doc: FormDataEntryValue | null;
         if(Number(cpf) > 999){
             doc = cpf;
         }else{
             doc = "erro";
         }
+
+        // fetch token
         const getToken = async () => {
             const tokenRes = await fetch(`https://sac.newlifefibra.com.br/mk/WSAutenticacao.rule?sys=MK0&token=004e1e43eeabb77526992637db07635a&password=3687777e73e3415&cd_servico=9999`);
             const tokenData = await tokenRes.json();
             return tokenData.Token;
         }
     
-        // consulta a API em busca do nome na base de clientes
         // passa o token para a variável
         const token = await getToken();
+        // fetch API (endpoint lista clientes: https://documentacao.mksolutions.com.br/display/MK30/APIs+gerais#APIsgerais-LISTARCLIENTES)
         const clientRes = await fetch(`https://sac.newlifefibra.com.br/mk/WSMKConsultaClientes.rule?sys=MK0&token=${token}&doc=${doc}`);
         const clientData = await clientRes.json();
 
+        /* 
+            tratamento de exceções:
+                - clientRes.ok: trata se a busca de dados retornou código 200 (ok) ou maior do de 299 (erro).
+                - clientData.status: forma como a API exibe o erro.
+        */
         let erro: Boolean;
         if(!clientRes.ok || clientData.status === 'ERRO'){
             erro = true;
@@ -31,8 +40,13 @@ export const actions: import('./$types').Actions = {
             erro = false
         }
         
+        // caso sucesso, retorna somente o primeiro nome do cliente para ser exibido, caso contrário, retorna erro (que é manipulado no front)
         if(!erro){
-            return { clientData }
+            let fullName = clientData[0].Nome;
+            let nomes = fullName.split(' ');
+            let nome = nomes[0];
+
+            return { nome }
         }else if(erro){
             return { erro}
         }
--- src/routes/newlife/+page.svelte
+++ src/routes/newlife/+page.svelte
@@ -1,18 +1,12 @@
 <script>
 	/*
 		SISTEMA PARA CONSULTA DE CLIENTES ATIVOS v0.0.1
-		- Consulta somente clientes ativos, de acordo com dados disponibilizados pela API oficial do MK.
-
+			- Consulta somente clientes ativos baseado no CPF informado e retorna o primeiro nome do mesmo.
+			- Baseado na API oficial do MK.
 	*/
+
 	/** @type {import('./$types').ActionData} */
 	export let form;
-	let nomes = "";
-	let firstName = "";
-	if(form?.clientData && !form?.erro){
-		let fullName = form?.clientData[0].Nome;
-		nomes = fullName.split(' ');
-		firstName = nomes[0];
-	}
 </script>
 <svelte:head>
 	<title>Base de clientes - New Life Fibra</title>
@@ -30,14 +24,14 @@
 		</div>
 		<div class="flex flex-col items-center gap-10 w-full">
 			<fieldset class="flex flex-col gap-2 w-full">
-				<label for="cpf" class="w-full text-center leading-snug md:leading-normal md:text-left font-bold md:text-lg">CPF (somente números, sem pontuação)</label>
+				<label for="cpf" class="w-full text-center leading-snug md:leading-normal md:text-left font-bold md:text-lg">CPF {'('}somente números, sem pontuação{')'}</label>
 				<input type="text" id="cpf" name="cpf" placeholder="Digite o CPF somente números..." class="w-full p-2 rounded-lg border border-black/20" required pattern="^[0-9]{'{11}'}$" maxlength="11">
 			</fieldset>
 			<button type="submit" class="btn btn-primary w-2/4 bg-azul-2 border-azul-3 hover:bg-azul-1 hover:border-azul-3">Conferir</button>
 		</div>
 		<div class="h-full max-h-[2rem]">
-			{#if form?.clientData && !form?.erro}
-				<p class="text-green-500 text-center"><span class="font-bold">{firstName}</span> é cliente ativo New Life Fibra!</p>
+			{#if form?.nome && !form?.erro}
+				<p class="text-green-500 text-center"><span class="font-bold">{form?.nome}</span> é cliente ativo New Life Fibra!</p>
 			{:else if form?.erro}
 				<p class="text-red-500 text-center"><span class="font-bold">CPF não encontrado</span> em nossa base de dados</p>
 			{/if}
